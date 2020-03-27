f5aedd1ee2c24616bb402df14e577b20c8e23e2c
idil kubat <idil.kubat44@hotmail.com>
2020-03-27 17:50:56 +0300

    Second Homework
    Second Homework

---
+++ SecondWeek/src/secondWeekHomework/FirstQuestion.java
@@ -0,0 +1,122 @@
+package secondWeekHomework;
+
+import java.util.ArrayList;
+
+public class FirstQuestion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] matrice = {
+				  {1,1,1,1,0},
+				  {1,1,0,1,0},
+				  {0,0,1,0,1},
+				  {1,0,0,1,1},
+				  {1,1,1,0,0}   	
+		};
+		
+		int[][] resultMatrice =new int[matrice.length][matrice[0].length];
+			
+		for(int i=0; i<matrice.length; i++) {
+			for(int j=0; j<matrice[0].length; j++) {
+				int positive=0, negative=0, a=0;
+				
+				if((i+1)<matrice.length) {
+					if(matrice[i+1][j]==1) {
+						positive= positive+1;
+					}
+					
+					else if(matrice[i+1][j]==0) {
+						negative= negative+1;
+					}
+				}
+				
+				if((j+1)<matrice[0].length) {
+					if(matrice[i][j+1]==1) {
+						positive= positive+1;
+					}
+					
+					else if(matrice[i][j+1]==0) {
+						negative= negative+1;
+					}
+				}
+				
+				
+				if((i-1)>=0) {
+					 if(matrice[i-1][j]==1) {
+			                positive= positive+1;
+			                }
+					 else if(matrice[i-1][j]==0) {
+			                negative= negative+1;
+					 }
+				}
+				
+				if((j-1)>=0) {
+					if(matrice[i][j-1]==1) {
+		                positive= positive+1;
+					}
+					else if (matrice[i][j-1]==0) {
+		                negative= negative+1;
+					}
+				
+				}
+
+				int result= Operation(matrice[i][j],positive,negative);
+				resultMatrice[i][j] = result;
+				
+				
+			}
+		} //end of for
+		
+		System.out.println("----First Matrice-----");
+        for(int i=0;i<matrice.length;i++)
+        {
+            for(int k=0;k<matrice[0].length;k++)
+            {
+                System.out.print(matrice[i][k]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("----Result Matrice-----");
+        for(int i=0;i<resultMatrice.length;i++)
+        {
+            for(int k=0;k<resultMatrice[0].length;k++)
+            {
+                System.out.print(resultMatrice[i][k]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+        
+
+	}
+	
+	public static int Operation(int a, int positive, int negative) {
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		 if(a==1) {
+			 if(positive<2) {
+		            a=0;
+			 }
+			 else if(positive==2 || positive==3) {
+		            a=1;
+			 }
+			 else if(positive>3) {
+		            a=0;
+			 }
+		 }
+		        
+		 else if(a==0) {
+		        if(positive==3) {
+		            a=1;
+		        }
+		 }
+		 
+		 return a;
+	}
+
+}
---
+++ SecondWeek/src/secondWeekHomework/ThirdQuestion.java
@@ -0,0 +1,63 @@
+package secondWeekHomework;
+
+import java.util.Random;
+
+public class ThirdQuestion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r=new Random(); //random sýnýfý
+		
+		int a=5, b=4, c=3, sum=0;
+		int matrix1[][] = new int[a][b];
+		int matrix2[][] = new int[b][c];
+		int result[][] = new int[a][c];
+
+		System.out.println("-----First matris-----");
+        for(int i=0;i<a;i++)
+        {
+            for(int k=0;k<b;k++)
+            {
+            	int random=r.nextInt(10);
+            	matrix1[i][k] = random;
+                System.out.print(matrix1[i][k]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("-----Second matris-----");
+        for(int i=0;i<b;i++)
+        {
+            for(int k=0;k<c; k++)
+            {
+            	int random=r.nextInt(10);
+            	matrix2[i][k] = random;
+                System.out.print(matrix2[i][k]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+		
+        for(int i = 0; i < matrix1.length; i++) {        
+            for(int j = 0; j < matrix2[0].length; j++) {     
+                for(int k = 0; k < matrix1[0].length; k++) { 
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        
+        System.out.println("-----Result matris-----");
+        for(int i=0;i<result.length;i++)
+        {
+            for(int k=0;k<result[0].length; k++)
+            {
+                System.out.print(result[i][k]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+	}
+
+}
